e1c8d395a354796a1e8f2e8f499938a771aa67ce
MITHU ROY <contact.mithuroy@gmail.com>
2026-02-02 23:35:35 +0600

    Update HomePage.ts

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,13 +1,13 @@
-export class HomePage {
-  username: string;
-  id: number;
+export class HomePage{
+    username:string;
+    id:number
 
-  constructor(username: string, id: number) {
-    this.username = username;
-    this.id = id;
-  }
+    constructor(username:string, id:number){
+        this.username = username;
+        this.id = id;
+    }
 
-  display(): void {
-    console.log(`User: ${this.username}`);
-  }
+     display():void{
+        console.log(`User: ${this.username}`);
+    }
 }
